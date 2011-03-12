17d782dfc7da81d6e77917b1cf4e6286a1684b00
Matthew Bowra-Dean <matthew@ijw.co.nz>
2011-03-12 22:27:38 +1300

    Sort chart on download stats page.

--- mysite/code/DownloadStatsPage.php
+++ mysite/code/DownloadStatsPage.php
@@ -14,21 +14,27 @@ class DownloadStatsPage_Controller extends Page_Controller {
 			$label = explode(" ", $download->Title);
 			$labels[] = $label[0];
 		}
+
+		$foo = array()
+
+		for ($i = 0; $i < count($labels); i++) {
+			$foo[$labels[i]] = $values[i]
+		}
+
+		rsort($foo)
 		
 		$url = "https://chart.googleapis.com/chart?cht=bvs&chd=t:" .
-			implode(",", $values); //Chart data
+			implode(",", array_values($foo)); //Chart data
 
-		sort($values);
-
-		$min = $values[0];
-		$max = $values[count($values) - 1];
+		$max = array_values($foo)[0];
+		$min = array_values($foo)[count($foo) - 1];
 
 		$url = $url . "&chds=0,$max&chxr=1,$min,$max"; //Chart scale
 
 		$url = $url . "&chs=400x200"; //Chart size
 
 		$url = $url . "&chxt=x,y&chxl=0:|" .
-			implode("|", $labels); //Chart labels
+			implode("|", array_keys($foo)); //Chart labels
 
 		$url = $url . "&chbh=50,15&chxs=0,FFFFFF,13,0,l,FFFFFF|1,FFFFFF,13,0,l,FFFFFF&chf=bg,s,000000&chtt=Download+Count&chts=FFFFFF,13";
 
