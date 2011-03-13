c994580eda29fca6418da2ab70916735bff15989
Matthew Bowra-Dean <matthew@ijw.co.nz>
2011-03-13 14:10:43 +1300

    More content validation of map.

--- mysite/code/MapRepository.php
+++ mysite/code/MapRepository.php
@@ -3,6 +3,8 @@
 class MapRepository_Controller extends Controller {
 
 	public static $allowed_max_file_size = 102400; //100KB
+	public static $allowed_max_internal_file_size = 51200; //50KB
+	public static $required_map_files = array('map.yaml', 'map.bin');
 
 	function upload($data) {
 		//Validate extension and size
@@ -31,14 +33,30 @@ class MapRepository_Controller extends Controller {
 			return;
 		}
 
-		if ($zip->locateName('map.yaml') === FALSE || 
-			$zip->locateName('map.bin') === FALSE) {
-			print('File is not valid. (Does not contain appropriate files)');
-			$zip->close();
-			return;
+		$content = '';
+
+		foreach (self::$required_map_files as $f) {
+			if ($zip->locateName($f) === FALSE) {
+				print('File is not valid.');
+				$zip->close();
+				return;
+			}
+
+			$stat = $zip->statName($f);
+			if ($stat['size'] > self::$allowed_max_internal_file_size) {
+				print('File is not valid.');
+				$zip->close();
+				return;
+			}
+
+			$content = $content . $zip->getFromName($f);
 		}
 
+		$hash = sha1($content);
+
 		$zip->close();
+
+		$tmp_file['name'] = $hash . '.oramap';
 
 		$folder = Folder::findOrMake('maps');
 
